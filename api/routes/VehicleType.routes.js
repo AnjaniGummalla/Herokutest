faefb3862bd116577f974772ace099d7c9b283d7
unknown <anjani513devi@gmail.com>
2020-09-24 14:50:43 +0530

    test case

--- api/routes/VehicleType.routes.js
+++ api/routes/VehicleType.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const VehicletypeController = require("../controllers/Vehicletype");
+const VehicletypeController = require("../controllers/vehicletype");
 
 router.get("/getlist", VehicletypeController.findAll);
 router.post("/create", VehicletypeController.create);
