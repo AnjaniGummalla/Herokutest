1be063501ddbb9fd0b317addaa1d38a661ec4d10
unknown <anjani513devi@gmail.com>
2020-09-24 15:59:26 +0530

    test case1

--- app.js
+++ app.js
@@ -66,7 +66,9 @@ app.use('/api/origin', OriginRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
+app.get('/testing', function (req, res) {
+  res.send('hello world')
+})
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
